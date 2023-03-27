f275d0b627bdccab09fd6105ca0662b608a71d9b
ressyressyl <cmaypantilo@gmail.com>
2023-03-27 13:51:50 +0800

    Added functionalities in CarouselItemController

---
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\CarouselItems;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarouselItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CarouselItems::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CarouselItemsRequest $request)
+    {
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::create($validated);
+
+        return $carouselItem;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return CarouselItems::findorfail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $carouselItem = CarouselItems::findorfail($id);
+
+        $carouselItem->update($validated);
+
+        return $carouselItem;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $carouselItem = CarouselItems::findorfail($id);
+
+        $carouselItem->delete();
+
+        return $carouselItem;
+    }
+
+}
